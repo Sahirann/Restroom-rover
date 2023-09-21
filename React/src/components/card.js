7887b4021845c8c23daa7b1f8eee4cc38ab01861
Sahirann <s6501012630182@email.kmtunb.ac.th>
2023-09-21 21:55:02 +0700

    Verifiy-bg

---
+++ React/src/components/card.js
@@ -0,0 +1,16 @@
+import { useCallback } from "react";
+import styles from "./card.scoped.css";
+
+const Bally = () => {
+  const onDirectionContainerClick = useCallback(() => {
+    // Please sync "direct to bally" to the project
+  }, []);
+
+  return (
+    <div className="card">
+       
+    </div>
+  );
+};
+
+export default card;
--- React/src/pages/MainPage.js
+++ React/src/pages/MainPage.js
@@ -2,6 +2,7 @@ import { useState, useCallback } from "react";
 import Menu from "../components/Menu";
 import PortalPopup from "../components/PortalPopup";
 import "./MainPage.scoped.css";
+import Card from "../components/card";
 
 const MainPage = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
@@ -15,46 +16,7 @@ const MainPage = () => {
   }, []);
 
   return (
-    <>
-      <div className={styles.MainPage}>
-        <img className={styles.mapIcon} alt="" src="/map.svg" />
-        <div className={styles.menu} onClick={openMenu}>
-          <img className={styles.vectorIcon} alt="" src="/vector.svg" />
-          <img className={styles.vectorIcon1} alt="" src="/vector1.svg" />
-        </div>
-        <div className={styles.searchAni}>
-          <div className={styles.searchToolWrapper}>
-            <div className={styles.searchTool}>
-              <div className={styles.search}>Search...</div>
-              <img className={styles.vectorIcon2} alt="" src="/vector2.svg" />
-            </div>
-          </div>
-          <div className={styles.rectangleParent}>
-            <div className={styles.groupChild} />
-            <div className={styles.mubanSirinpark}>MuBan Sirinpark</div>
-            <div className={styles.theLife}>The Life</div>
-            <div className={styles.kmutnb}>Kmutnb</div>
-            <div className={styles.ngamwongwan}>Ngamwongwan</div>
-            <div className={styles.smartBall}>Smart Ball</div>
-            <div className={styles.maiyakfsignal}>MaiyakFsignal</div>
-            <div className={styles.groupItem} />
-            <div className={styles.groupInner} />
-            <div className={styles.lineDiv} />
-            <div className={styles.groupChild1} />
-            <div className={styles.groupChild2} />
-          </div>
-        </div>
-      </div>
-      {isMenuOpen && (
-        <PortalPopup
-          overlayColor="rgba(113, 113, 113, 0.3)"
-          placement="Centered"
-          onOutsideClick={closeMenu}
-        >
-          <Menu onClose={closeMenu} />
-        </PortalPopup>
-      )}
-    </>
+    <div>Hello</div>
   );
 };
 
