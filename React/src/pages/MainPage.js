5f54efc494d0ae7bd8e816986287642686cc259d
KKawinn <s6501012610025@email.kmutnb.ac.th>
2023-09-22 00:25:39 +0700

    mapmaleaw

--- React/src/pages/MainPage.js
+++ React/src/pages/MainPage.js
@@ -1,7 +1,18 @@
+import React from "react";
 import { useState, useCallback } from "react";
 import Menu from "../components/Menu";
 import PortalPopup from "../components/PortalPopup";
 import "./MainPage.scoped.css";
+import { MapContainer, TileLayer, Marker,Popup,useMap } from 'react-leaflet'
+import "leaflet/dist/leaflet.css";
+import "./MainPage.css"
+
+function MyComponent() {
+  const map = useMap()
+  console.log('map center:', map.getCenter())
+  return null
+}
+
 
 const MainPage = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
@@ -14,37 +25,19 @@ const MainPage = () => {
     setMenuOpen(false);
   }, []);
 
+  const position = [51.505, -0.09]
+
   return (
-    <>
-      <div className={styles.MainPage}>
-        <img className={styles.mapIcon} alt="" src="/map.svg" />
-        <div className={styles.menu} onClick={openMenu}>
-          <img className={styles.vectorIcon} alt="" src="/vector.svg" />
-          <img className={styles.vectorIcon1} alt="" src="/vector1.svg" />
-        </div>
-        <div className={styles.searchAni}>
-          <div className={styles.searchToolWrapper}>
-            <div className={styles.searchTool}>
-              <div className={styles.search}>Search...</div>
-              <img className={styles.vectorIcon2} alt="" src="/vector2.svg" />
-            </div>
-          </div>
-          <div className={styles.rectangleParent}>
-            <div className={styles.groupChild} />
-            <div className={styles.mubanSirinpark}>MuBan Sirinpark</div>
-            <div className={styles.theLife}>The Life</div>
-            <div className={styles.kmutnb}>Kmutnb</div>
-            <div className={styles.ngamwongwan}>Ngamwongwan</div>
-            <div className={styles.smartBall}>Smart Ball</div>
-            <div className={styles.maiyakfsignal}>MaiyakFsignal</div>
-            <div className={styles.groupItem} />
-            <div className={styles.groupInner} />
-            <div className={styles.lineDiv} />
-            <div className={styles.groupChild1} />
-            <div className={styles.groupChild2} />
-          </div>
-        </div>
+    <div>
+      <div>
+        <MapContainer center={[48.8566, 2.3522]} zoom={13}> 
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </MapContainer>
       </div>
+      
       {isMenuOpen && (
         <PortalPopup
           overlayColor="rgba(113, 113, 113, 0.3)"
@@ -54,7 +47,7 @@ const MainPage = () => {
           <Menu onClose={closeMenu} />
         </PortalPopup>
       )}
-    </>
+    </div>
   );
 };
 
