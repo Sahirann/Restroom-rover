6db2b9b2052735b9a10e75765d0377173d7a6936
Sahirann <s6501012630182@email.kmtunb.ac.th>
2023-09-22 04:07:36 +0700

    back_end

--- React/src/components/card.js
+++ React/src/components/card.js
@@ -3,19 +3,19 @@ import styles from "./Card.scoped.css";
 import ReactStars from "react-rating-stars-component";
 
 function Card(props) {
-  const { name,address,point,count_comment } = props
+  const { data } = props
   
   return (
     <div className="card">
-      <h6 className="restroom-name">{name??"The life"}</h6>
-      <p className="address">{address??"addressddsdsdsdsashdasjkdhajksdhasd"}</p>
+      <h6 className="restroom-name">{data?.name??"Name"}</h6>
+      <p className="address">{data?.address??"address"}</p>
       <div className="container-point">
-        <p className="point">{point.toFixed(1)??"point"}</p>
-        <ReactStars count={5} edit={false} value={point??3.5} isHalf={true} size={16} activeColor="#ffd700"/>
-        <p className="comment">{count_comment??"(X)"}</p>
+        <p className="point">{data?.point?.toFixed(1)??"point"}</p>
+        <ReactStars count={5} edit={false} value={data?.point??3.5} isHalf={true} size={16} activeColor="#ffd700"/>
+        <p className="comment">{`(${data?.count_comment??"X"})`}</p>
       </div>
       <div className="container-pic">
-        <img src="https://www.sloan.com/themes/sloan/img/sensor-tech/article-restroom-design-hero.jpg?v=5" alt="restroom" className="pic" />
+        <img src={data?.picture??"https://clicxy.com/wp-content/uploads/2016/04/dummy-post-horisontal.jpg"} alt="restroom" className="pic" />
       </div>
       <div className="container-button">
         <button className="direction-button">direction</button>
--- React/src/pages/MainPage.js
+++ React/src/pages/MainPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useCallback } from "react";
+import { useState, useCallback ,useEffect} from "react";
 import Menu from "../components/Menu";
 import PortalPopup from "../components/PortalPopup";
 import "./MainPage.scoped.css";
@@ -7,6 +7,7 @@ import Card from "../components/Card";
 import { MapContainer, TileLayer, Marker,Popup,useMap } from 'react-leaflet'
 import "leaflet/dist/leaflet.css";
 import "./MainPage.css"
+import axios from "axios"
 
 function MyComponent() {
   const map = useMap()
@@ -28,16 +29,29 @@ const MainPage = () => {
   }, []);
 
   const position = [51.505, -0.09]
+  const [detail, setdetail] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3001/").then((response) => {
+      setdetail(response.data);
+      console.log("update");
+    }).catch((err) => { console.log(err) });
+  }, []);
+  const detailcard = detail.map((data, index) => {
+    return <Card key={index} data={data} ></Card>
+  })
 
   return (
     <div>
+      
       <div>
-        <MapContainer center={[48.8566, 2.3522]} zoom={13}> 
+        {detailcard}
+        <MapContainer center={[13.822670, 100.520928]} zoom={15}> 
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
         </MapContainer>
+        
       </div>
       
       {isMenuOpen && (
